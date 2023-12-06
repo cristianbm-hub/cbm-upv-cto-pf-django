e62c99ccc028e872aefef72eff09afe4f56a15f5
Cristian <cribauma@gmail.com>
2023-12-06 11:29:09 +0000

    Modelo Soldado

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Soldado(models.Model):
+    GRADO_CHOICES = [
+        ('R', 'Recluta'),
+        ('S', 'Soldado'),
+        ('C', 'Cabo'),
+        ('Sgto', 'Sargento'),
+        # Agregar más opciones según sea necesario
+    ]
+
+    nombre = models.CharField(max_length=100)
+    rango = models.CharField(max_length=10, choices=GRADO_CHOICES)
+    fecha_nacimiento = models.DateField()
+    fecha_ingreso = models.DateField()
+    unidad = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100)
+    grupo_sanguineo = models.CharField(max_length=5)
+    alergias = models.TextField(blank=True, null=True)
+    condiciones_medicas = models.TextField(blank=True, null=True)
